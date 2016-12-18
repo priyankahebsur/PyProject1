12308f5f3fda133511b51345bf6bbfde28f8cbd8
RanjanRohit <rrohit1995@gmail.com>
2016-12-18 22:38:14 +0530

    String prog

--- 1.py
+++ 1.py
@@ -1,5 +1,10 @@
 import sys
-print("Hi. Test code")
-for m in sys.modules:
-        print(m)
+#print("Hi. Test code")
+#for m in sys.modules:
+        #print(m)
+#We start with Hangman
+mytuple=("hangman","ant","cat","dog","sheep","spider")
+#tuple can't be updated
+str=input("What's the word?" )
+print(str)
 print("done finally")
